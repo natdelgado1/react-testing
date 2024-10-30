83b5ab3f5edfbdc581f3a45aaf6dd35949a43748
Wilmer Garzon <wilmerj1996@gmail.com>
2024-10-30 12:49:29 -0500

    feat: config msw

--- src/setupTest.ts
+++ src/setupTest.ts
@@ -1,1 +1,7 @@
 import '@testing-library/jest-dom';
+import { afterAll, afterEach, beforeAll } from 'vitest';
+import { server } from './mocks/server';
+
+beforeAll(() => server.listen());
+afterEach(() => server.restoreHandlers());
+afterAll(() => server.close());
