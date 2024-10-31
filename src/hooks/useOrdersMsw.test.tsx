684b49e9c6b2a1614a7fd872317b56b7a426637c
Wilmer Garzon <wilmerj1996@gmail.com>
2024-10-31 10:07:47 -0500

    feat: add use test to endpoint error

--- src/hooks/useOrdersMsw.test.tsx
+++ src/hooks/useOrdersMsw.test.tsx
@@ -1,40 +1,59 @@
-import { describe, it, expect, vi, beforeEach, Mock } from 'vitest';
-import { MemoryRouter } from 'react-router-dom';
-import { renderHook } from '@testing-library/react-hooks';
-import { useOrders } from './useOrders';
-import { SessionProvider, useSession } from '../context/AuthContext';
+import { describe, it, expect, vi, beforeEach, Mock } from "vitest";
+import { MemoryRouter } from "react-router-dom";
+import { renderHook } from "@testing-library/react-hooks";
+import { http, HttpResponse } from "msw";
+import { server } from "../mocks/server";
+import { useOrders } from "./useOrders";
+import { SessionProvider, useSession } from "../context/AuthContext";
 
-vi.mock('../context/AuthContext', async() => {
-    const actual = await vi.importActual('../context/AuthContext');
-    return {
-        ...actual,
-        useSession: vi.fn()
-    }
-})
+vi.mock("../context/AuthContext", async () => {
+  const actual = await vi.importActual("../context/AuthContext");
+  return {
+    ...actual,
+    useSession: vi.fn(),
+  };
+});
 
-describe('useOrders MSW', () => {
-    const mockUser = { id: "1", name: "Wilmer Garzon" };
+describe("useOrders MSW", () => {
+  const mockUser = { id: "1", name: "Wilmer Garzon" };
 
-    beforeEach(() => {
-        (useSession as Mock).mockReturnValue({ user: mockUser })
-    })
+  beforeEach(() => {
+    (useSession as Mock).mockReturnValue({ user: mockUser });
+  });
 
-    const wrapper = ({ children }: { children: React.ReactNode }) => (
-        <SessionProvider>
-            <MemoryRouter>
-                {children}
-            </MemoryRouter>
-        </SessionProvider>
-    )
+  const wrapper = ({ children }: { children: React.ReactNode }) => (
+    <SessionProvider>
+      <MemoryRouter>{children}</MemoryRouter>
+    </SessionProvider>
+  );
 
-    it('debe obtener good la data', async () => {
-        const { result, waitForNextUpdate } = renderHook(() => useOrders(), { wrapper })
-        const initialLoading = result.current.loading;
-        expect(initialLoading).toBe(true);
-        
-        await waitForNextUpdate();
+  it("debe obtener good la data", async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useOrders(), {
+      wrapper,
+    });
+    const initialLoading = result.current.loading;
+    expect(initialLoading).toBe(true);
 
-        const lengthOrders = result.current.orders.length;
-        expect(lengthOrders).toBe(1);
-    })
-})
+    await waitForNextUpdate();
+
+    const lengthOrders = result.current.orders.length;
+    expect(lengthOrders).toBe(1);
+  });
+
+  it("debe obtener un error", async () => {
+    server.use(
+      http.get("http://localhost:3001/orders", () => {
+        return new HttpResponse(null, {
+          status: 500,
+          statusText: "Internal Server Error",
+        });
+      })
+    );
+    const { result, waitForNextUpdate } = renderHook(() => useOrders(), {
+      wrapper,
+    });
+    await waitForNextUpdate();
+    const error = result.current.error;
+    expect(error).toBe("Failed to fetch orders. Please try again later.");
+  });
+});
