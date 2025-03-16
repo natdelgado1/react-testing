ebe9512f5070726fd50b1de567d07fa85b4180f8
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2025-03-16 04:27:23 -0300

    feat: agregar pruebas unitarias para el componente Login, incluyendo redirección y manejo de visibilidad de contraseña

--- src/containers/Login/Login.test.tsx
+++ src/containers/Login/Login.test.tsx
@@ -1,28 +1,38 @@
 import { describe, it, expect, vi, Mock } from 'vitest';
-import { fireEvent, render, screen, act } from '@testing-library/react';
+import { fireEvent, render, screen, act, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { getAuth } from '../../services/getAuth';
 import { SessionProvider } from '../../context/AuthContext'
 import { Login } from './Login';
 
+
+vi.mock('react-router-dom', async () => {
+    const actual = await vi.importActual('react-router-dom')
+    return {
+        ...actual,
+        useNavigate: () => mockNavigate
+    }
+})
 vi.mock('../../services/getAuth', () => {
     return {
         getAuth: vi.fn()
     }
 })
 
+const mockNavigate = vi.fn()
 const mockGetAuth = getAuth as Mock
 
 describe('<Login/>', () => {
+    const handleLogin = () => {
+        return render(<SessionProvider>
+            <MemoryRouter>
+                <Login />
+            </MemoryRouter>
+        </SessionProvider>)
+    }
     it('Debería mostrar un mensaje de error ', async () => {
         mockGetAuth.mockRejectedValue(new Error("Invalid credentials"));
-        render(
-            <SessionProvider>
-                <MemoryRouter>
-                    <Login />
-                </MemoryRouter>
-            </SessionProvider>
-        )
+        handleLogin()
 
         const userNameInput = screen.getByPlaceholderText('Username');
         const passwordInput = screen.getByPlaceholderText('Password');
@@ -36,4 +46,36 @@ describe('<Login/>', () => {
         expect(errorMessage).toBeInTheDocument();
 
     })
-})
+
+    it('Debería redirigir a /orders', async () => {
+        mockGetAuth.mockResolvedValue({ success: true });
+        handleLogin()
+        const userNameInput = screen.getByPlaceholderText('Username');
+        const passwordInput = screen.getByPlaceholderText('Password');
+        const buttonLogin = screen.getByRole('button', { name: 'Login' });
+
+        await act(async () => {
+            fireEvent.change(userNameInput, { target: { value: 'validUser' } })
+            fireEvent.change(passwordInput, { target: { value: 'validPassword' } })
+            fireEvent.click(buttonLogin)
+        })
+        await waitFor(() => {
+            expect(mockGetAuth).toHaveBeenCalledWith("validUser", "validPassword")
+            expect(mockNavigate).toHaveBeenCalledWith("/orders")
+        })
+
+    })
+
+    //Caso de uso: Debería mostrar la contraseña solo showPassword es hide 
+    it("Debería cambiar el tipo del input cuando se muestra la contraseña", async () => {
+        handleLogin()
+        const passwordInput = screen.getByPlaceholderText('Password');
+        const togglePasswordButton = screen.getByRole('button', { name: 'show' })
+        await act(async () => {
+            fireEvent.click(togglePasswordButton)
+        })
+        expect(passwordInput).toHaveAttribute('type', 'text')
+    })
+}
+)
+
