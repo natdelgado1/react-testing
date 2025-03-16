ac16871a730c97dbe5c6411f83f010ed600123a9
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2025-03-16 05:24:07 -0300

    feat: agregar hook personalizado useUserOrders para gestionar el estado y la carga de órdenes del usuario

---
+++ src/hooks/userOrders.tsx
@@ -0,0 +1,40 @@
+import { useCallback, useEffect, useState } from "react";
+import { useSession } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { getOrders } from "../services/getOrders";
+import { Order } from "../types/Orders";
+
+export const useUserOrders = () => {
+    const [orders, setOrders] = useState<Order[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+    const { user } = useSession();
+    const navigate = useNavigate();
+  
+    useEffect(() => {
+      if (!user) {
+        navigate("/");
+      }
+    }, [user, navigate]);
+  
+    const fetchOrders = useCallback(async () => {
+      try {
+        setLoading(true);
+        const data = await getOrders();
+        setOrders(data);
+        setError(null);
+      } catch (err) {
+        setError("Failed to fetch orders. Please try again later.");
+        console.error("Failed to fetch orders:", err);
+      } finally {
+        setLoading(false);
+      }
+    }, []);
+  
+    useEffect(() => {
+      if(user) fetchOrders();
+    }, [fetchOrders, user]);
+
+    return { orders, loading, error, user };
+}
+
