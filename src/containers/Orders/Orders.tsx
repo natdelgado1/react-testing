3232356bcbf8443792bfe0bee0b4d7c3b03b574c
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2025-03-16 05:23:23 -0300

    refactor: simplificar la gestión de órdenes en el componente Orders utilizando un hook personalizado para manejar el estado y la carga de datos

--- src/containers/Orders/Orders.tsx
+++ src/containers/Orders/Orders.tsx
@@ -1,43 +1,13 @@
-import React, { useState, useEffect, useCallback } from "react";
-import { getOrders } from "../../services/getOrders";
-import { Order } from "../../types/Orders";
 import { OrderSummary } from "../../components/OrderSummary";
-import { useSession } from "../../context/AuthContext";
 import { handleValidateSuperAdmin } from "../../utils/validateRole";
 import { OrderItem } from "../../components/OrderItem";
-import { useNavigate } from "react-router-dom";
 import classes from "./Orders.module.scss";
+import { useUserOrders } from '../../hooks/userOrders'
 
 export const Orders: React.FC = () => {
-  const [orders, setOrders] = useState<Order[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-  const { user } = useSession();
-  const navigate = useNavigate();
+  const { orders, loading, error , user} = useUserOrders();
 
-  useEffect(() => {
-    if (!user) {
-      navigate("/");
-    }
-  }, [user, navigate]);
-
-  const fetchOrders = useCallback(async () => {
-    try {
-      setLoading(true);
-      const data = await getOrders();
-      setOrders(data);
-      setError(null);
-    } catch (err) {
-      setError("Failed to fetch orders. Please try again later.");
-      console.error("Failed to fetch orders:", err);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
-
-  useEffect(() => {
-    if(user) fetchOrders();
-  }, [fetchOrders, user]);
+ 
 
   if (!user) {
     return null;
