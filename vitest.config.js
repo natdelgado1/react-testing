ddc666da0d5983b6bb65d845196118d83d17c61a
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2025-03-17 01:57:54 -0300

    chore: actualizar configuración de Vite y Vitest, añadiendo soporte para cobertura de pruebas y eliminando archivos obsoletos

---
+++ vitest.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+    test: {
+        environment: 'jsdom',
+        globals: true,
+        setupFiles: ['./src/setupTest.ts'],
+        coverage: {
+            exclude: [
+                '**/*.config.ts',
+                '**/*.config.js',
+                '**/*.types.ts',
+                '**/*.d.ts',
+                '**/types',
+                '**/App.tsx',
+                '**/main.tsx'
+            ],
+            thresholds: {
+                functions: 80
+            }
+        }
+    }
+})
