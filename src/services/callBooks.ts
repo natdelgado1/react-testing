667937346efb379202a4f52fbd8be5f9a72b2028
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2025-03-17 01:59:05 -0300

    chore: eliminar archivo obsoleto callBooks.ts que ya no se utiliza en el proyecto

--- src/services/callBooks.ts
+++
@@ -1,5 +0,0 @@
-export async function getFirstAlbumTitle() {
-  const data = await fetch("https://jsonplaceholder.typicode.com/albums");
-  const response = await data.json();
-  return response?.[0].title;
-}
