9a5262799011db531af5b0fd77f16f602b25ef5a
Natalia Delgado <144268969+natdelgado1@users.noreply.github.com>
2025-03-17 01:58:03 -0300

    feat: configurar el servidor de pruebas con Vitest, añadiendo hooks para iniciar y cerrar el servidor antes y después de las pruebas

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,18 @@
 import '@testing-library/jest-dom';
+import {afterAll, afterEach, beforeAll} from 'vitest';
+import {server} from './mocks/server';
+
+//Antes de todos los test se ejecuta este codigo
+beforeAll(()=>{
+    server.listen()
+})
+
+//Despues de cada test se ejecuta este codigo se  reinicia el servidor
+afterEach(()=>{
+    server.resetHandlers()
+})
+
+//Despues de todos los test se ejecuta este codigo se cierra el servidor
+afterAll(()=>{
+    server.close()
+})
